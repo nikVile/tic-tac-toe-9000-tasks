28fcbfa689aacf2ea121f7ab6f41bb74ddcbe96e
nikVile <58167196+nikVile@users.noreply.github.com>
2021-03-01 09:40:40 +0300

    tests passed, ready for pull request

--- game_engine/tic_tac_toe_game.py
+++ game_engine/tic_tac_toe_game.py
@@ -1,41 +1,105 @@
-from typing import Callable
 from copy import deepcopy
-
+from typing import Callable, List, Optional
 from .tic_tac_toe_common_lib import TicTacToeTurn, TicTacToeGameInfo, AbstractTicTacToeGame
 
+
 class TicTacToeGame(AbstractTicTacToeGame):
-    def __init__(self, game_id: str, first_player_id: str, second_player_id: str,
-                 strategy: Callable[[TicTacToeGameInfo], TicTacToeTurn] = None) -> None:
-        self.__game_id = game_id
-        self.__first_player_id = first_player_id
-        self.__second_player_id = second_player_id
-        self.__winner_id = ""
-        self.__strategy = strategy
-        self.__turns = []
-
-    def is_turn_correct(self, turn: TicTacToeTurn) -> bool:
-        pass
-
-    def do_turn(self, turn: TicTacToeTurn) -> TicTacToeGameInfo:
-        pass
-
-    def get_game_info(self) -> TicTacToeGameInfo:
-        result = TicTacToeGameInfo(
-            game_id=self.__game_id,
-            field=[
-                [" ", " ", " "],
-                [" ", " ", " "],
-                [" ", " ", " "]
+    def __init__(
+        self,
+        game_id,
+        first_player_id,
+        second_player_id
+    ):
+        self._player_id = first_player_id
+        self.TicTacToeGameInfo = TicTacToeGameInfo(
+            game_id = game_id,
+            field = [
+            [" ", " ", " "],
+            [" ", " ", " "],
+            [" ", " ", " "] 
             ],
-            sequence_of_turns=deepcopy(self.__turns),
-            first_player_id=self.__first_player_id,
-            second_player_id=self.__second_player_id,
-            winner_id=self.__winner_id
+            sequence_of_turns = [],
+            first_player_id = first_player_id,
+            second_player_id = second_player_id,
+            winner_id = ""
         )
-        for turn in self.__turns:
-            if turn.player_id == self.__first_player_id:
-                ch = "X"
+
+    def is_turn_correct(self, TicTacToeTurn):
+        X = TicTacToeTurn.x_coordinate
+        Y = TicTacToeTurn.y_coordinate
+
+        if self.TicTacToeGameInfo.winner_id == "":
+            if self._player_id == TicTacToeTurn.player_id:
+                if (X >= 0 and X < 3) and (Y >= 0 and Y < 3):
+                    if self.TicTacToeGameInfo.field[X][Y] == " ":
+                        return True
+                    else:
+                        return False
+                else:
+                    return False
             else:
-                ch = "O"
-            result.field[turn.x_coordinate][turn.x_coordinate] = ch
-        return result
+                return False
+        else:
+            return False
+
+
+    def do_turn(self, TicTacToeTurn):
+        X = TicTacToeTurn.x_coordinate
+        Y = TicTacToeTurn.y_coordinate
+
+        if self.is_turn_correct(TicTacToeTurn):
+            if TicTacToeTurn.player_id == self.TicTacToeGameInfo.first_player_id:
+                self.TicTacToeGameInfo.field[X][Y] = "X"
+                self._player_id = self.TicTacToeGameInfo.second_player_id
+            
+            elif TicTacToeTurn.player_id == self.TicTacToeGameInfo.second_player_id:
+                self.TicTacToeGameInfo.field[X][Y] = "O"
+                self._player_id = self.TicTacToeGameInfo.first_player_id
+
+
+
+            self.TicTacToeGameInfo.sequence_of_turns.append(TicTacToeTurn)
+
+            self._set_winner()
+
+            return self.TicTacToeGameInfo
+        else:
+            return self.TicTacToeGameInfo
+
+    
+    def get_game_info(self):
+        return deepcopy(self.TicTacToeGameInfo)
+
+    def _set_winner(self):
+        for elem in self.TicTacToeGameInfo.field:
+            if elem == ["X", "X", "X"]:
+                self.TicTacToeGameInfo.winner_id = self.TicTacToeGameInfo.first_player_id
+            elif elem == ["O", "O", "O"]:
+                self.TicTacToeGameInfo.winner_id = self.TicTacToeGameInfo.second_player_id
+
+    
+        for i in range(3):
+            column = []
+            for j in range(3):
+                column.append(self.TicTacToeGameInfo.field[j][i])
+            
+            if column == ["X", "X", "X"]:
+                self.TicTacToeGameInfo.winner_id = self.TicTacToeGameInfo.first_player_id
+            elif column == ["0", "0", "0"]:
+                self.TicTacToeGameInfo.winner_id = self.TicTacToeGameInfo.second_player_id
+
+        FirstDiagonal = []
+        SecondDiagonal = []
+        i = 0
+        for i in range(3):
+            FirstDiagonal.append(self.TicTacToeGameInfo.field[i][i])
+        
+        i = 0
+        for i in range(3):
+            SecondDiagonal.append(self.TicTacToeGameInfo.field[i][2-i])
+
+        if FirstDiagonal == ["X", "X", "X"] or SecondDiagonal == ["X", "X", "X"]:
+            self.TicTacToeGameInfo.winner_id = self.TicTacToeGameInfo.first_player_id
+        
+        elif FirstDiagonal == ["0", "0", "0"] or SecondDiagonal == ["0", "0", "0"]:
+             self.TicTacToeGameInfo.winner_id = self.TicTacToeGameInfo.second_player_id
